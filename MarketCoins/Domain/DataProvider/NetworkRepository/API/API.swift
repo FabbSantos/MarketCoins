ef5d886ce0d9f34918765c633d9460d879557caf
Fabricio Bahiense <fabriciobs2000@gmail.com>
2023-10-09 18:34:06 -0400

    coinsrouter

--- MarketCoins/Domain/DataProvider/NetworkRepository/API/API.swift
+++ MarketCoins/Domain/DataProvider/NetworkRepository/API/API.swift
@@ -10,7 +10,7 @@ import Foundation
 struct API {
     static let baseURL = "https://api.coingecko.com/api/v3"
     static let coinsMarkets = "/coins/markets"
-    static let coindByMarketChart = "/coins/%@/market_chart/range"
+    static let coinsByMarketChart = "/coins/%@/market_chart/range"
     static let coinsByIdOlhc = "/coins/%@/ohlc"
     static let global = "/global"
     static let coinsById = "/coins/%@"
